bf77a90aeb89351a0c041164dd9abb0746bcc73f
Jaxon Adams <jadamsresume1@gmail.com>
2022-07-20 17:43:23 -0600

    finished hero

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -4,6 +4,7 @@ const Hero = () => {
     return (
         <div className='hero'>
             <h2 className='hero-title'>Steak, Chicken, Fish</h2>
+            <p className='hero-txt'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente corrupti ea veniam ad, asperiores, ipsam perferendis aliquid explicabo quo molestiae odit voluptatibus inventore accusamus magni maxime dolor vel! Vitae ipsum fugiat vero impedit neque sequi ratione accusamus suscipit praesentium. Suscipit.</p>
         </div>
     );
 };
