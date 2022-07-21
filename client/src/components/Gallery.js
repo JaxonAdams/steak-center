b751d1add8f7ffbcfd720f0d0f4332dd4b648791
Jaxon Adams <jadamsresume1@gmail.com>
2022-07-21 14:35:37 -0600

    began work on gallery

---
+++ client/src/components/Gallery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div className='gallery'>
+            <img className='food-img food-img-one' src={require('../assets/images/steak.jpg')} alt='A delicious steak' />
+            <img className='food-img food-img-two' src={require('../assets/images/chicken.jpg')} alt='A delicious chicken' />
+            <img className='food-img food-img-three' src={require('../assets/images/fish.jpg')} alt='A delicious fish' />
+        </div>
+    );
+};
+
+export default Gallery;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import Gallery from '../components/Gallery';
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Hero />
+            <Gallery />
         </div>
     );
 };
