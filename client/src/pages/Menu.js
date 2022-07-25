d2ae4d434e2e697df30ad150e4d2c47d5e9c88cf
Jaxon Adams <jadamsresume1@gmail.com>
2022-07-25 16:08:58 -0600

    fixed nav on menu page

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../components/Header';
+import Navigation from '../components/Navigation';
 
 const Menu = () => {
+    const [menuShowing, setMenuShowing] = useState(false);
+    
     return (
         <div>
-            <Header />
+            <Header menuShowing={menuShowing} setMenuShowing={setMenuShowing} />
+            <Navigation menuShowing={menuShowing} />
             Hello World
         </div>
     );
